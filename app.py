82fefded2ab820227cc502e26ed02d58aabcb424
rtusiime <rtusiime@u.rochester.edu>
2021-12-01 14:56:06 -0500

    made functional deck

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from enum import unique
+
 from flask import Flask, render_template, redirect, url_for
+from flask.wrappers import Response
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
+from werkzeug.wrappers import response
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from sassutils.wsgi import SassMiddleware
+import requests
 
 
 app = Flask(__name__)
@@ -17,6 +21,18 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    'app': ('static/sass', 'static/css', '/static/css')
+})
+arr = []
+response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=300")
+response_json = response.json()
+resp_array = response_json['results']
+for i in resp_array:
+    response = requests.get(i['url'])
+    res = response.json()
+    arr.append(res)
+
 
 
 #user info table for database
@@ -61,12 +77,14 @@ def login():
 
     return render_template('login.html', form= form)
 
+    
+
 @app.route('/signup', methods= ['GET','POST'])
 def signup():
     form = SignupForm()
 
     if form.validate_on_submit():
-        hashed_password =generate_password_hash(form.password.data, method= 'sha256');
+        hashed_password =generate_password_hash(form.password.data, method= 'sha256')
         new_user = Users(username= form.username.data, email= form.email.data, password= hashed_password)
         db.session.add(new_user)
         db.session.commit()
@@ -78,6 +96,11 @@ def signup():
 @login_required
 def dashboard():
     return render_template('dashboard.html', name = current_user.username)
+
+@app.route('/deck')
+@login_required
+def deck():
+    return render_template('deck.html', name = current_user.username, pokedata = arr)
 
 @app.route('/logout')
 @login_required
